ad8311180aa9bc07ce8115c58931bec03352ed22
harshitrai8178 <raiharshit786@gmail.com>
2025-07-17 05:44:04 +0000

    programs

---
+++ 5num.js
@@ -0,0 +1,18 @@
+// const balance = new Number(299)
+// console.log(balance)
+// const number=10000.888
+// console.log(number.toPrecision(3))
+// console.log(number.toFixed(4))
+// console.log(number.toLocaleString('en-IN'))
+
+//math//
+
+// console.log(Math.random())
+// console.log((Math.random()*10)+1)
+// console.log(Math.floor(Math.random()*10)+1)
+
+const min = 1
+const max= 6
+
+console.log(Math.floor(Math.random()*(max - min +  min))+1)
+
