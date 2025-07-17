d1aad67e841e545adebb0c5a52bd6f33f598d87d
harshitrai8178 <raiharshit786@gmail.com>
2025-07-17 05:19:04 +0000

    strings

---
+++ strings.js
@@ -0,0 +1,12 @@
+const name="harshit"
+const repocount=80
+
+console.log(`hello my name is ${name} and my repo count is ${repocount}`);
+const gamename= new String("hello")
+console.log(gamename.length)
+console.log(gamename.toUpperCase())
+console.log(gamename.charAt(3))
+console.log(gamename.indexOf("t"))
+
+const url="hellomyname40"
+console.log(url.replace('40','~'))
