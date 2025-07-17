3e2a7141642d008d990e3ae5a9e6e8f8f1045479
harshitrai8178 <raiharshit786@gmail.com>
2025-07-17 06:21:17 +0000

    date

---
+++ 06date.js
@@ -0,0 +1,8 @@
+// let mydate=new Date()
+// console.log(mydate.toString())
+// console.log(mydate.toLocaleDateString())
+
+let mecreate = new Date(2003,09,09)
+console.log(mecreate.toDateString())
+ let mytime =  Date.now()
+ console.log(Math.floor(mytime.toString()))
